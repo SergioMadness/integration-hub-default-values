d2a567bbbc1ab1b6de6b4afcbcc6b0a817d4b131
Сергей Зинченко <you@example.com>
2022-07-03 17:58:45 +0300

    Register subsystem in pool

--- src/Providers/DefaultValuesProvider.php
+++ src/Providers/DefaultValuesProvider.php
@@ -1,11 +1,24 @@
 <?php namespace professionalweb\IntegrationHub\DefaultValues\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use professionalweb\IntegrationHub\DefaultValues\Models\DefaultValuesOptions;
 use professionalweb\IntegrationHub\DefaultValues\Services\DefaultValuesSubsystem;
+use professionalweb\IntegrationHub\IntegrationHubCommon\Interfaces\Services\SubsystemPool;
 use professionalweb\IntegrationHub\DefaultValues\Interfaces\DefaultValuesSubsystem as IDefaultValuesSubsystem;
 
 class DefaultValuesProvider extends ServiceProvider
 {
+    public function boot(): void
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'IntegrationHubDefaultValues');
+
+        $this->app->booted(static function () {
+            /** @var SubsystemPool $pool */
+            $pool = app(SubsystemPool::class);
+            $pool->register(trans('IntegrationHubDefaultValues::common.name'), DefaultValuesSubsystem::SUBSYSTEM_ID, new DefaultValuesOptions());
+        });
+    }
+
     public function register(): void
     {
         $this->app->register(EventServiceProvider::class);
